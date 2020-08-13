2b1a929373ed034bd2b4726001fc30d66f82978f
kmio11 <64621731+kmio11@users.noreply.github.com>
2020-08-13 10:09:56 +0900

    Fix pkg loading error printed to parser.log

--- pkg/generator/parser/loader.go
+++ pkg/generator/parser/loader.go
@@ -26,9 +26,8 @@ func (p *Parser) LoadPackage(patterns ...string) error {
 		p.log.Println(err)
 		return err
 	}
-	e := packages.PrintErrors(pkgs)
-	if e > 0 && p.stopLoadErr {
-		err := fmt.Errorf("error occuerd when loading package")
+	err = p.PrintErrors(pkgs)
+	if err != nil {
 		p.log.Println(err)
 		return err
 	}
@@ -46,4 +45,34 @@ func (p *Parser) LoadPackage(patterns ...string) error {
 		Files: pkg.Syntax,
 	}
 	return nil
+}
+
+// PrintErrors prints to logger the accumulated errors of all
+// packages in the import graph rooted at pkgs, dependencies first.
+// PrintErrors returns error if  1 or more errors printed and stopLoadErr is true.
+func (p *Parser) PrintErrors(pkgs []*packages.Package) error {
+	errs := p.PkgErrors(pkgs)
+	if len(errs) > 0 {
+		for _, err := range errs {
+			p.log.Printf("[WARN] %s\n", err)
+		}
+		if p.stopLoadErr {
+			err := fmt.Errorf("error occuerd when loading package")
+			return err
+		}
+		p.log.Println("[WARN] error occuerd when loading package")
+	}
+	return nil
+}
+
+// PkgErrors returns the accumulated errors of all
+// packages in the import graph rooted at pkgs, dependencies first.
+func (p *Parser) PkgErrors(pkgs []*packages.Package) []error {
+	errs := []error{}
+	packages.Visit(pkgs, nil, func(pkg *packages.Package) {
+		for _, err := range pkg.Errors {
+			errs = append(errs, err)
+		}
+	})
+	return errs
 }
